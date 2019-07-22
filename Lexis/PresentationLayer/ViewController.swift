72a8f84566c8303dccc0835a5765c70d9cd089d0
Beslan Tularov <>
2019-07-23 01:52:46 +0300

    init

---
+++ Lexis/PresentationLayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lexis
+//
+//  Created by Beslan Tularov on 23/07/2019.
+//  Copyright © 2019 Beslan Tularov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
